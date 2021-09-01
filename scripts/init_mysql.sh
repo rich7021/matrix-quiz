867fd6df1a1fea42f144066531724247e060914d
rich7021 <coconutfu@gmail.com>
2021-09-01 16:56:36 +0800

    add delyed start for app, using external spring properties

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -10,8 +10,20 @@
 
 chmod 644 /etc/my.cnf
 
-sed -i 's/${APP_USER}/'${APP_USER}'/g' /app/app/src/main/resources/matrix_quiz.dml
-sed -i 's/${APP_PWD}/'${APP_PWD}'/g' /app/app/src/main/resources/matrix_quiz.dml
+ddl_path='/app/app/src/main/resources/matrix_quiz.ddl'
+dml_path='/app/app/src/main/resources/matrix_quiz.dml'
+tmp_ddl='/app/config/tmp_matrix_quiz.ddl'
+tmp_dml='/app/config/tmp_matrix_quiz.dml'
 
-mysql -h${DB_HOST} -u${ROOT_USER} -p${MYSQL_ROOT_PASSWORD} </app/app/src/main/resources/matrix_quiz.ddl
-mysql -h${DB_HOST} -u${ROOT_USER} -p${MYSQL_ROOT_PASSWORD} </app/app/src/main/resources/matrix_quiz.dml
+cp $ddl_path $tmp_ddl
+cp $dml_path $tmp_dml
+
+sed -i 's/${APP_USER}/'${APP_USER}'/g' $tmp_dml
+sed -i 's/${APP_PWD}/'${APP_PWD}'/g' $tmp_dml
+
+
+mysql -h${DB_HOST} -u${ROOT_USER} -p${MYSQL_ROOT_PASSWORD} <$tmp_ddl
+mysql -h${DB_HOST} -u${ROOT_USER} -p${MYSQL_ROOT_PASSWORD} <$tmp_dml
+
+rm -f $tmp_ddl
+rm -f $tmp_dml
